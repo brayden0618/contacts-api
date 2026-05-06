85d4f977d79246600664924ab2e12a96f5ae65a9
Brayden Wayman <bwayman@byupathway.edu>
2026-05-06 14:01:43 -0600

    fixes

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 
+const mongodb = require('./data/database');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// routes FIRST
+app.use('/', require('./routes'));
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-const mongodb = require('./data/database');
 
 mongodb.initDb((err) => {
   if (err) {
@@ -25,5 +24,3 @@ mongodb.initDb((err) => {
     });
   }
 });
-
-app.use('/', require('./routes'));
