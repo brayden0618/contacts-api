aa1102538537e3b9957b83981ed01f45aefd2504
Brayden Wayman <bwayman@byupathway.edu>
2026-05-06 13:58:38 -0600

    changes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -6,6 +6,7 @@ const getAll = async (req, res) => {
   try {
     const result = await mongodb.getDb().collection('contacts').find();
     const contacts = await result.toArray();
+
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(contacts);
   } catch (err) {
@@ -17,11 +18,19 @@ const getAll = async (req, res) => {
 const getSingle = async (req, res) => {
   try {
     const id = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().collection('contacts').find({ _id: id });
-    const contact = await result.toArray();
+
+    const contact = await mongodb
+      .getDb()
+      .collection('contacts')
+      .findOne({ _id: id });
+
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
 
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(contact[0]);
+    res.status(200).json(contact);
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
