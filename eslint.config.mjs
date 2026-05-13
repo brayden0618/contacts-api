bd7cc1d0e06c530c74e998af21eab7a4782bb558
Brayden Wayman <bwayman@byupathway.edu>
2026-05-13 09:11:12 -0600

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,10 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import pluginPrettier from "eslint-plugin-prettier";
+import pluginTypescript from "@typescript-eslint/eslint-plugin";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js, prettier: pluginPrettier },  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"] },
+  { files: ["**/*.{ts,tsx}"], plugins: { js, typescript: pluginTypescript, prettier: pluginPrettier },  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"] },
 ]);
